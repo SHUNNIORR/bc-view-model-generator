1d078bb0c9f295f0fc79c1a2df437657ac157da4
joperez <joperez@bancolombia.com.co>
2024-07-19 13:47:25 -0500

    feat: ajustes de creación de template de clean architecture

--- src/generate-clean-architecture-template/generate-clean-architecture-template.js
+++ src/generate-clean-architecture-template/generate-clean-architecture-template.js
@@ -6,12 +6,14 @@ function generateCleanArchitectureTemplate(uri) {
     const domainPath = path.join(folderPath, 'domain');
     const applicationPath = path.join(folderPath, 'application');
     const infrastructurePath = path.join(folderPath, 'infrastructure');
+    const gatewaysPath = path.join(infrastructurePath, 'gateways'); // Nueva ruta para la carpeta 'gateways'
 
     fs.mkdirSync(domainPath, { recursive: true });
     fs.mkdirSync(applicationPath, { recursive: true });
     fs.mkdirSync(infrastructurePath, { recursive: true });
+    fs.mkdirSync(gatewaysPath, { recursive: true }); // Crear la carpeta 'gateways'
 
-    vscode.window.showInformationMessage(`Carpetas 'domain', 'application', 'infrastructure' creadas en ${folderPath}`);
+    vscode.window.showInformationMessage(`Carpetas 'domain', 'application', 'infrastructure' y 'gateways' creadas en ${folderPath}`);
 }
 
 module.exports = {generateCleanArchitectureTemplate};
